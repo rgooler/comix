6049bff1f4036934c560bfaf7362cabde60ca96c
Ryan Gooler <rgooler@salesforce.com>
2014-05-12 15:05:16 -0700

    Upgrading to basic comic listing ability

--- app/views.py
+++ app/views.py
@@ -1,10 +1,9 @@
 from flask import render_template
 from app import app
+import os
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = { 'nickname': 'Miguel' } # fake user
-    return render_template("index.html",
-        title = 'Home',
-        user = user)
+    comics = [x[0].replace('res/','',1) for x in os.walk('res/')][1:]
+    return render_template("index.html", comics=comics)
