fb5f37f677779e164802683e09e05d98032de123
Ryan Gooler <rgooler@salesforce.com>
2014-05-12 15:18:25 -0700

    Add a favicon

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from flask import render_template
 from app import app
 import os
+from flask import send_from_directory
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/x-icon')
 
 @app.route('/')
 @app.route('/index')
