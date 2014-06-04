272fd996e92d7c756485dcd5f02202efa1b4015b
Jippen Faddoul <jippen@nfs.thisstuffismine.com>
2014-06-03 22:58:24 -0700

    Adding uwsgi dep

--- setup.cmd
+++ setup.cmd
@@ -2,3 +2,4 @@ rmdir /f virtualenv
 virtualenv virtualenv
 virtualenv/bin/pip install flask==0.10.1
 virtualenv/bin/pip install natsort==3.2.0
+virtualenv/bin/pip install uwsgi
--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ rm -rf virtualenv
 virtualenv virtualenv
 virtualenv/bin/pip install flask==0.10.1
 virtualenv/bin/pip install natsort==3.2.0
+virtualenv/bin/pip install uwsgi
