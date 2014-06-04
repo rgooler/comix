911fe0f9161183906c833dc341fb8e6e73597f51
Jippen Faddoul <jippen@nfs.thisstuffismine.com>
2014-06-03 22:52:54 -0700

    Configuring to work with uWSGI

--- run.py
+++ run.py
@@ -2,4 +2,5 @@
 from app import app
 import config as config
 
-app.run(port=5000, debug=config.DEBUG, host='0.0.0.0')
+if __name__ == "__main__"
+    app.run(port=5000, debug=config.DEBUG, host='0.0.0.0')
